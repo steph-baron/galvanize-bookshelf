5f214ca45a94d04b2c35824909f9969855148a30
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-19 16:25:21 -0400

    added users migration

---
+++ migrations/20171019161934_users.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.string('email').unique().notNullable();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
